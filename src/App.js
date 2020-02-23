c576346901f795c497ce7218b32ae8e06f24a708
shanelonergan <sptlonergan@gmail.com>
2020-02-23 18:54:06 -0500

    added checkout and ontoken

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import StripeCheckout from 'react-stripe-checkout';
 
 export default function App() {
+
+    const onToken = (token) => {
+
+        const config = {
+            method: 'POST',
+            body: JSON.stringify(token),
+        }
+
+        fetch('http://localhost3000/charges', config)
+        .then(res => res.json())
+        .then(console.log)
+    }
+
     return (
         <div>
             <h1>Stripe Example Project</h1>
+            <StripeCheckout
+                token={onToken}
+                stripeKey=''
+            />
         </div>
     )
 }
