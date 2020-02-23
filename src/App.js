3be40323daedfb054b8ada56f16fc0eff7ab2c71
shanelonergan <sptlonergan@gmail.com>
2020-02-23 18:46:29 -0500

    added stipe package

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function App() {
     return (
         <div>
-            hello world
+            <h1>Stripe Example Project</h1>
         </div>
     )
 }
